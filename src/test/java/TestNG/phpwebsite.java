00335a77429e7d599f95d23a3546d3dee4029082
edureka <edureka@slave.ibm.com>
2019-10-16 16:11:15 +0530

    Commit

---
+++ src/test/java/TestNG/phpwebsite.java
@@ -0,0 +1,40 @@
+package TestNG;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class phpwebsite{
+	 WebDriver driver;
+	@BeforeMethod
+	public void launch() {
+		System.setProperty("webdriver.chrome.driver", "/home/edureka/chromedriver");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("disable-popup-blocking");
+		driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.get("http://192.168.56.102:9000/");
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	}
+	@Test
+      public void verifyphpwebsite() {
+		// TODO Auto-generated method stub
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//a[@href='/content/about-us.php']")).click();
+  }
+	@AfterMethod
+	public void close() {
+
+		//driver.close();
+	}		
+}
+
